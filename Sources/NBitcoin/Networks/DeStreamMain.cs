2edbb66320fc80ee4e569bc1249c8931ad360a29
Pavel Pavlov <pavel.pavlov.itstf@gmail.com>
2018-07-17 12:00:32 +0300

    Added classes DeStreamMain, DeStreamTest, DeStreamRegTest

--- Sources/NBitcoin/Networks/DeStreamMain.cs
+++ Sources/NBitcoin/Networks/DeStreamMain.cs
@@ -9,15 +9,6 @@ namespace NBitcoin.Networks
 {
     public class DeStreamMain : Network
     {
-        /// <summary> The name of the root folder containing the different Stratis blockchains (StratisMain, StratisTest, StratisRegTest). 	</summary>
-        public const string DeStreamRootFolderName = "destream";
-
-        /// <summary> The default name used for the Stratis configuration file. </summary>
-        public const string DeStreamDefaultConfigFilename = "destream.conf";
-
-        public const int StratisMaxTimeOffsetSeconds = 25 * 60;
-
-        public const int StratisDefaultMaxTipAgeInSeconds = 2 * 60 * 60;
 
         public DeStreamMain()
         {
@@ -131,39 +122,5 @@ namespace NBitcoin.Networks
                 this.SeedNodes.Add(addr);
             }
         }
-        private static Block CreateStratisGenesisBlock(ConsensusFactory consensusFactory, uint nTime, uint nNonce, uint nBits, int nVersion, Money genesisReward)
-        {
-            string pszTimestamp = "http://www.theonion.com/article/olympics-head-priestess-slits-throat-official-rio--53466";
-            return CreateStratisGenesisBlock(consensusFactory, pszTimestamp, nTime, nNonce, nBits, nVersion, genesisReward);
-        }
-
-        private static Block CreateStratisGenesisBlock(ConsensusFactory consensusFactory, string pszTimestamp, uint nTime, uint nNonce, uint nBits, int nVersion, Money genesisReward)
-        {
-            Transaction txNew = consensusFactory.CreateTransaction();
-            txNew.Version = 1;
-            txNew.Time = nTime;
-            txNew.AddInput(new TxIn()
-            {
-                ScriptSig = new Script(Op.GetPushOp(0), new Op()
-                {
-                    Code = (OpcodeType)0x1,
-                    PushData = new[] { (byte)42 }
-                }, Op.GetPushOp(Encoders.ASCII.DecodeData(pszTimestamp)))
-            });
-            txNew.AddOutput(new TxOut()
-            {
-                Value = genesisReward,
-            });
-            Block genesis = consensusFactory.CreateBlock();
-            genesis.Header.BlockTime = Utils.UnixTimeToDateTime(nTime);
-            genesis.Header.Bits = nBits;
-            genesis.Header.Nonce = nNonce;
-            genesis.Header.Version = nVersion;
-            genesis.Transactions.Add(txNew);
-            genesis.Header.HashPrevBlock = uint256.Zero;
-            genesis.UpdateMerkleRoot();
-            return genesis;
-        }
-
     }
 }
