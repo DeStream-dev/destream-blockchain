1e7757b686732f64c5be1e0bd7df019f451e4288
Pavel Pavlov <pavel.pavlov.itstf@gmail.com>
2018-07-26 22:22:25 +0300

    code research and analysis Stratis

--- Sources/DeStream.DeStreamD.ForTest/Program.cs
+++ Sources/DeStream.DeStreamD.ForTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using DeStream.Stratis.Bitcoin.Configuration;
 using NBitcoin;
@@ -18,6 +20,7 @@ using Stratis.Bitcoin.IntegrationTests.Common;
 using Stratis.Bitcoin.IntegrationTests.Common.EnvironmentMockUpHelpers;
 using Stratis.Bitcoin.Interfaces;
 using Stratis.Bitcoin.Utilities;
+using static Stratis.Bitcoin.BlockPulling.BlockPuller;
 
 namespace DeStream.DeStreamD.ForTest
 {
@@ -27,7 +30,97 @@ namespace DeStream.DeStreamD.ForTest
         {
             MainAsync(args).Wait();
         }
+        private class TransactionNode
+        {
+            public uint256 Hash = null;
+            public Transaction Transaction = null;
+            public List<TransactionNode> DependsOn = new List<TransactionNode>();
 
+            public TransactionNode(Transaction tx)
+            {
+                this.Transaction = tx;
+                this.Hash = tx.GetHash();
+            }
+        }
+
+        private static List<Transaction> Reorder(List<Transaction> transactions)
+        {
+            if (transactions.Count == 0)
+                return transactions;
+
+            var result = new List<Transaction>();
+            Dictionary<uint256, TransactionNode> dictionary = transactions.ToDictionary(t => t.GetHash(), t => new TransactionNode(t));
+            foreach (TransactionNode transaction in dictionary.Select(d => d.Value))
+            {
+                foreach (TxIn input in transaction.Transaction.Inputs)
+                {
+                    TransactionNode node = dictionary.TryGet(input.PrevOut.Hash);
+                    if (node != null)
+                    {
+                        transaction.DependsOn.Add(node);
+                    }
+                }
+            }
+
+            while (dictionary.Count != 0)
+            {
+                foreach (TransactionNode node in dictionary.Select(d => d.Value).ToList())
+                {
+                    foreach (TransactionNode parent in node.DependsOn.ToList())
+                    {
+                        if (!dictionary.ContainsKey(parent.Hash))
+                            node.DependsOn.Remove(parent);
+                    }
+
+                    if (node.DependsOn.Count == 0)
+                    {
+                        result.Add(node.Transaction);
+                        dictionary.Remove(node.Hash);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static Block[] GenerateStratis(IFullNode node, BitcoinSecret dest, int blockCount, List<Transaction> passedTransactions = null, bool broadcast = true)
+        {
+            FullNode fullNode = (FullNode)node;
+            //BitcoinSecret dest = this.MinerSecret;
+            var blocks = new List<Block>();
+            //DateTimeOffset now = this.MockTime == null ? DateTimeOffset.UtcNow : this.MockTime.Value;
+            
+            for (int i = 0; i < blockCount; i++)
+            {
+                uint nonce = 0;
+                var block = new Block();
+                block.Header.HashPrevBlock = fullNode.Chain.Tip.HashBlock;
+                block.Header.Bits = block.Header.GetWorkRequired(fullNode.Network, fullNode.Chain.Tip);
+                block.Header.UpdateTime(DateTimeOffset.UtcNow, fullNode.Network, fullNode.Chain.Tip);
+                var coinbase = new Transaction();
+                coinbase.AddInput(TxIn.CreateCoinbase(fullNode.Chain.Height + 1));
+                coinbase.AddOutput(new TxOut(fullNode.Network.GetReward(fullNode.Chain.Height + 1), dest.GetAddress()));
+                block.AddTransaction(coinbase);
+                if (passedTransactions?.Any() ?? false)
+                {
+                    passedTransactions = Reorder(passedTransactions);
+                    block.Transactions.AddRange(passedTransactions);
+                }
+                block.UpdateMerkleRoot();
+                while (!block.CheckProofOfWork())
+                    block.Header.Nonce = ++nonce;
+                blocks.Add(block);
+                if (broadcast)
+                {
+                    uint256 blockHash = block.GetHash();
+                    var newChain = new ChainedHeader(block.Header, blockHash, fullNode.Chain.Tip);
+                    ChainedHeader oldTip = fullNode.Chain.SetTip(newChain);
+                    fullNode.ConsensusLoop().Puller.InjectBlock(blockHash, new DownloadedBlock { Length = block.GetSerializedSize(), Block = block }, CancellationToken.None);
+                }
+            }
+
+            return blocks.ToArray();
+        }
         public static async Task MainAsync(string[] args)
         {
             try
@@ -43,7 +136,7 @@ namespace DeStream.DeStreamD.ForTest
                 Console.WriteLine($"current network: {network.Name}");
 
                 // NOTES: running BTC and STRAT side by side is not possible yet as the flags for serialization are static
-                IFullNode node = new FullNodeBuilder()
+                FullNode node = (FullNode)new FullNodeBuilder()
                     .UseNodeSettings(nodeSettings)
                     .UseBlockStore()
                     .UsePosConsensus()
@@ -53,18 +146,25 @@ namespace DeStream.DeStreamD.ForTest
                     .UseApi()
                     .AddRPC()
                     .Build();
+                if (node != null)
+                    await node.RunAsync();
 
-                //(node.NodeService<IInitialBlockDownloadState>() as InitialBlockDownloadStateMock).SetIsInitialBlockDownload(false, DateTime.UtcNow.AddMinutes(5));
+                (node.NodeService<IInitialBlockDownloadState>() as InitialBlockDownloadStateMock).SetIsInitialBlockDownload(false, DateTime.UtcNow.AddMinutes(5));
 
-                //Mnemonic _mnemonic1 = node.NodeService<IWalletManager>().CreateWallet("123456", "mywallet");
+                Mnemonic _mnemonic1 = node.NodeService<IWalletManager>().CreateWallet("123456", "mywallet");
                 //Mnemonic _mnemonic2 = node.NodeService<IWalletManager>().CreateWallet("123456", "mywallet");
-                //HdAddress _addr = node.NodeService<IWalletManager>().GetUnusedAddress(new WalletAccountReference("mywallet", "account 0"));
-                //Wallet _wallet = node.NodeService<IWalletManager>().GetWalletByName("mywallet");
-                //Key key = _wallet.GetExtendedPrivateKeyForAddress("123456", _addr).PrivateKey;
+                HdAddress _addr = node.NodeService<IWalletManager>().GetUnusedAddress(new WalletAccountReference("mywallet", "account 0"));
+                Wallet _wallet = node.NodeService<IWalletManager>().GetWalletByName("mywallet");
+                Key _key = _wallet.GetExtendedPrivateKeyForAddress("123456", _addr).PrivateKey;
 
-                //stratisSender.SetDummyMinerSecret(new BitcoinSecret(key, stratisSender.FullNode.Network));
-                //int maturity = (int)stratisSender.FullNode.Network.Consensus.CoinbaseMaturity;
-                //stratisSender.GenerateStratis(maturity + 5);
+                BitcoinSecret bitcoinSecret = new BitcoinSecret(_key, node.Network);
+
+
+                int _maturity = (int)node.Network.Consensus.CoinbaseMaturity;
+
+                GenerateStratis(node, bitcoinSecret, 10);
+
+                TestHelper.WaitLoop(() => TestHelper.IsNodeSynced((FullNode)node));
                 //// wait for block repo for block sync to work
 
                 //TestHelper.WaitLoop(() => TestHelper.IsNodeSynced(stratisSender));
@@ -82,7 +182,6 @@ namespace DeStream.DeStreamD.ForTest
                 //    new WalletAccountReference("mywallet", "account 0"), "123456", sendto.ScriptPubKey, Money.COIN * 100, FeeType.Medium, 101);
 
                 //Transaction trx = walletTransactionHandler.BuildTransaction(transactionBuildContext);
-
 
 
                 int qwe = 1;
